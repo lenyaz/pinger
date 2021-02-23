19fbea29c192bcd22c5e084426b859e625858a5a
Lenya Veraksa <lenyaz@outlook.com>
2021-02-23 22:45:26 +0300

    added: buggy icon, makefile zip achive creation, some simple icos

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 main.exe: main.cs
-	mcs main.cs
+	mcs main.cs -r:System.Windows.Forms -r:System.Drawing
 	
 run: main.cs
-	mcs main.cs && mono main.exe 8.8.8.8
+	mcs main.cs -r:System.Windows.Forms -r:System.Drawing && mono main.exe 8.8.8.8
+	
+zip: main.cs
+	mcs main.cs -r:System.Windows.Forms -r:System.Drawing && zip prog.zip main.exe ok.ico error.ico
--- main.cs
+++ main.cs
@@ -3,12 +3,15 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace unet
 {
 	public class PingExample
 	{
 		public static void Pinger(){
+			const int sleepTime = 2000;
 			string ipAddress = "10.74.5.200";
 			Ping pingSender = new Ping ();
 			PingOptions options = new PingOptions ();
@@ -17,9 +20,29 @@ namespace unet
 			byte[] buffer = Encoding.ASCII.GetBytes (data);
 			int timeout = 120;
 			PingReply reply = pingSender.Send (ipAddress, timeout, buffer, options);
+			
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			NotifyIcon notifyIcon1 = new NotifyIcon();
+			ContextMenu contextMenu1 = new ContextMenu();
+			MenuItem menuItem0 = new MenuItem();
+			contextMenu1.MenuItems.AddRange(new MenuItem[] { menuItem0 });
+			menuItem0.Index = 0;
+			menuItem0.Text = "close";
+			menuItem0.Click += new EventHandler(menuItem1_Click);
+			notifyIcon1.Visible = true;
+			//Application.Run();
+			
+			//notifyIcon1.Icon = null;
+			
+			Icon okIcon = new Icon("ok.ico");
+			
 			if (reply.Status == IPStatus.Success)
 			{
 				Console.WriteLine("ok");
+				//notifyIcon1.Icon = new Icon("ok.ico"); //TODO: 
+				notifyIcon1.Icon = okIcon; //TODO: 
+				//notifyIcon1.Icon = null;
 				//Console.WriteLine ("Address: {0}", reply.Address.ToString ());
 				//Console.WriteLine ("RoundTrip time: {0}", reply.RoundtripTime);
 				//Console.WriteLine ("Time to live: {0}", reply.Options.Ttl);
@@ -28,11 +51,17 @@ namespace unet
 			}
 			else {
 				Console.WriteLine("error");
+				notifyIcon1.Icon = new Icon("error.ico");
 			}
+			notifyIcon1.Dispose();
+			Thread.Sleep(sleepTime);
 		}
 		
 		
+		
 		public static void Main ()
-		{ while (true) { Pinger(); Thread.Sleep(2000); } }
+		{ while (true) { Pinger();  ;} }
+		
+		private static void menuItem1_Click(object Sender, EventArgs e){ Application.Exit(); }
 	}
 }
